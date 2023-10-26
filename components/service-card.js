efc3bf617f0ac870e6698871c1ac5748da8dccca
dorakueYaritai <kotto555555@gmail.com>
2023-10-27 00:18:02 +0900

    clear indent

--- components/service-card.js
+++ components/service-card.js
@@ -1,12 +1,12 @@
 const ServiceCard = ({ title, description, thumbnail }) => {
     return (
     <div className="w-[351px] h-[326px] relative">
-    <img className="w-[351px] h-[204px] absolute" src={thumbnail} />
-    <div className="w-[351px] h-[122px] left-0 top-[204px] absolute bg-zinc-600 shadow">
-        <div className="left-[21px] top-[15px] absolute text-gray-200 text-[19px] font-bold font-['Inter'] leading-7">{title}</div>
-        <div className="w-56 h-[68px] left-[21px] top-[43px] absolute text-gray-200 text-[13px] font-bold font-['Inter'] leading-7">{description}</div>
-        <div className="w-4 h-4 left-[311px] top-[20px] absolute" />
-    </div>
+        <img className="w-[351px] h-[204px] absolute" src={thumbnail} />
+        <div className="w-[351px] h-[122px] left-0 top-[204px] absolute bg-zinc-600 shadow">
+            <div className="left-[21px] top-[15px] absolute text-gray-200 text-[19px] font-bold font-['Inter'] leading-7">{title}</div>
+            <div className="w-56 h-[68px] left-[21px] top-[43px] absolute text-gray-200 text-[13px] font-bold font-['Inter'] leading-7">{description}</div>
+            <div className="w-4 h-4 left-[311px] top-[20px] absolute" />
+        </div>
     </div>    
 )
 }
