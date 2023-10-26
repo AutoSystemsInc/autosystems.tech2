61848e580b72b9d0912f0798e04c9c456483b124
dorakueYaritai <kotto555555@gmail.com>
2023-10-26 22:45:27 +0900

    top component

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,11 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+    <div className="w-[1439px] h-[848px] px-[170px] py-[60px] bg-neutral-50 justify-center items-center gap-[60px] inline-flex">
+      <div className="grow shrink basis-0 flex-col justify-start items-center gap-6 inline-flex">
+        <div className="w-[799px] text-center text-black text-[40px] font-bold font-['Inter'] leading-[48px]">AutoSystemsの使命は、<br/>世界中のシステムを自動化し、<br/>世界中の人がアクセスできて<br/>使えるようにすることです。</div>
+      </div>
+    </div>
 
 
     <Works></Works>
