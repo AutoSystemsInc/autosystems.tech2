2bcc90fdb6ebd255158271c07ecb6ea8ba583197
dorakueYaritai <kotto555555@gmail.com>
2023-10-27 00:08:28 +0900

    Top component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Works from '../../components/works'
 import Service from '../../components/service'
 import NewsCard from '../../components/news-card'
 import News from '../../components/news'
+import Top from '../../components/top'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,17 +17,10 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="w-[1440px] h-[4864px] bg-zinc-100 flex-col justify-start items-center inline-flex">
-        <div className="w-[1439px] h-[848px] px-[170px] py-[60px] bg-neutral-50 justify-center items-center gap-[60px] inline-flex">
-          <div className="grow shrink basis-0 flex-col justify-start items-center gap-6 inline-flex">
-            <div className="w-[799px] text-center text-black text-[40px] font-bold font-['Inter'] leading-[48px]">AutoSystemsの使命は、<br/>世界中のシステムを自動化し、<br/>世界中の人がアクセスできて<br/>使えるようにすることです。</div>
-          </div>
-        </div>
-
-
+        <Top></Top>
         <Works></Works>
         <Service></Service>
         <News></News>
-
         {//<!-- Contact-->
         }
         <section className="page-section" id="contact">
