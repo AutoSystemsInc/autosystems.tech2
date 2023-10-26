a8db66946c573e3d9e0c60ba4ca5b69be100146c
dorakueYaritai <kotto555555@gmail.com>
2023-10-26 23:52:45 +0900

    newsCard links to the topic's url

--- components/news-card.js
+++ components/news-card.js
@@ -1,8 +1,10 @@
 
-const NewsCard = ({ title, description, url, urlToImage , date}) => {
+const NewsCard = ({ title, urlToOneNews, urlToImage , date}) => {
     return (
         <div className="w-[352px] h-[313px] flex-col justify-center items-start gap-[15px] inline-flex">
-            <img className="self-stretch grow shrink basis-0 shadow" src="https://via.placeholder.com/352x247" />
+            <a href={urlToOneNews}>
+                <img className="self-stretch grow shrink basis-0 shadow" src={urlToImage} />
+            </a>
             <div className="self-stretch text-black text-[19px] font-bold font-['Inter'] leading-tight">{title}</div>
             <div className="self-stretch text-neutral-500 text-base font-normal font-['Inter'] leading-none">{date}</div>
         </div>
