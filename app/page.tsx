8ed7d52986512e61a7a80b2bfda7bd4c314f3fda
dorakueYaritai <kotto555555@gmail.com>
2023-10-28 22:39:08 +0900

    nextUI setting ( not work well )

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+import {NextUIProvider} from "@nextui-org/react";
 import AboutUs from "components/about-us";
 import Layout from "components/layout";
 import News from "components/news";
@@ -10,32 +12,34 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => (
-  <Layout>
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="w-[1440px] h-[4864px] bg-zinc-100 flex-col justify-start items-center inline-flex">
-        <Top></Top>
-        <Works></Works>
-        <Service></Service>
-        <AboutUs></AboutUs>
-        <News></News>
-        {
-          //<!-- Contact-->
-        }
-        <section className="page-section" id="contact">
-          <div className="container">
-            <Script src="https://sdk.form.run/js/v2/embed.js" />
-            <div
-              className="formrun-embed"
-              data-formrun-form="@s-hara-1682018099"
-              data-formrun-redirect="true"
-            ></div>
-          </div>
-        </section>
-      </div>
-    </main>
-  </Layout>
+  <NextUIProvider>
+    <Layout>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <div className="w-[1440px] h-[4864px] bg-zinc-100 flex-col justify-start items-center inline-flex">
+          <Top></Top>
+          <Works></Works>
+          <Service></Service>
+          <AboutUs></AboutUs>
+          <News></News>
+          {
+            //<!-- Contact-->
+          }
+          <section className="page-section" id="contact">
+            <div className="container">
+              <Script src="https://sdk.form.run/js/v2/embed.js" />
+              <div
+                className="formrun-embed"
+                data-formrun-form="@s-hara-1682018099"
+                data-formrun-redirect="true"
+              ></div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </Layout>
+  </NextUIProvider>
 );
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -14,5 +15,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
