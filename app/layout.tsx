3643d0b15e22dc5ea5d825bfc48f2244a4256455
dorakueYaritai <kotto555555@gmail.com>
2023-10-28 22:25:24 +0900

    prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import { Inter } from "next/font/google";
-import './globals.css';
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const App = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
-    <body className={inter.className + "h-full"}>
-        {children}
-    </body>
+    <body className={inter.className + "h-full"}>{children}</body>
   </html>
 );
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Layout from "components/layout"
-import Script from 'next/script'
-import Works from 'components/works'
-import Service from 'components/service'
-import NewsCard from 'components/news-card'
-import News from 'components/news'
-import Top from 'components/top'
-import AboutUs from 'components/about-us'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Layout from "components/layout";
+import Script from "next/script";
+import Works from "components/works";
+import Service from "components/service";
+import NewsCard from "components/news-card";
+import News from "components/news";
+import Top from "components/top";
+import AboutUs from "components/about-us";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Layout>
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="w-[1440px] h-[4864px] bg-zinc-100 flex-col justify-start items-center inline-flex">
-        <Top></Top>
-        <Works></Works>
-        <Service></Service>
-        <AboutUs></AboutUs>
-        <News></News>
-        {//<!-- Contact-->
-        }
-        <section className="page-section" id="contact">
-          <div className="container">
-            <Script src="https://sdk.form.run/js/v2/embed.js"/>
-            <div
-              className="formrun-embed"
-              data-formrun-form="@s-hara-1682018099"
-              data-formrun-redirect="true">
-            </div>                
-          </div>
-        </section>
-      </div>
-    </main>
-
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <div className="w-[1440px] h-[4864px] bg-zinc-100 flex-col justify-start items-center inline-flex">
+          <Top></Top>
+          <Works></Works>
+          <Service></Service>
+          <AboutUs></AboutUs>
+          <News></News>
+          {
+            //<!-- Contact-->
+          }
+          <section className="page-section" id="contact">
+            <div className="container">
+              <Script src="https://sdk.form.run/js/v2/embed.js" />
+              <div
+                className="formrun-embed"
+                data-formrun-form="@s-hara-1682018099"
+                data-formrun-redirect="true"
+              ></div>
+            </div>
+          </section>
+        </div>
+      </main>
     </Layout>
-  )
+  );
 }
