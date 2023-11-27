4bfe308c7b8b0ea73044b2c209bf17134d2f02ba
dorakueYaritai <kotto555555@gmail.com>
2023-11-27 10:52:38 +0900

    hover animation service-card pop up

--- components/service-card.tsx
+++ components/service-card.tsx
@@ -7,7 +7,7 @@ const ServiceCard = ({
   description: string;
   thumbnail: string;
 }) => (
-  <div className="w-[351px] h-[326px] relative drop-shadow-2xl">
+  <div className="w-[351px] h-[326px] relative drop-shadow-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
     <img className="w-[351px] h-[204px] absolute" src={thumbnail} />
     <div className="w-[351px] h-[122px] left-0 top-[204px] absolute bg-zinc-600 shadow">
       <div className="left-[21px] top-[15px] absolute text-gray-200 text-[19px] font-bold font-['Inter'] leading-7">
