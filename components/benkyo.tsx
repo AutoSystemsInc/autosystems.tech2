99a7dd3cd41de36f80a9db2827346e3c2ba32f38
rumirumi18 <77532665+rumirumi18@users.noreply.github.com>
2024-12-24 18:34:16 +0900

    Reactコンポーネントとしてbenkyo.tsxを追加

---
+++ components/benkyo.tsx
@@ -0,0 +1,13 @@
+// components/benkyo.tsx
+import React from 'react';
+
+const Benkyo: React.FC = () => {
+  return (
+    <div>
+      <h1>勉強中！</h1>
+      <p>これはReactコンポーネントです。</p>
+    </div>
+  );
+};
+
+export default Benkyo;
