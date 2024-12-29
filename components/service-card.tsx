4cb68d09e07bd8fdaeddda0895e9b8dd105ad052
Yuki Kawazoe <yukikawazoe@MacBook-Pro-8.local>
2024-12-29 18:03:32 +0900

    サイトの改修

--- components/service-card.tsx
+++ components/service-card.tsx
@@ -2,13 +2,21 @@ const ServiceCard = ({
   title,
   description,
   thumbnail,
+  url, // URLを追加
 }: {
   title: string;
   description: string;
   thumbnail: string;
+  url: string; // URL型を指定
 }) => (
-  <div className="w-[351px] h-[326px] relative drop-shadow-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-    <img className="w-[351px] h-[204px] absolute" src={thumbnail} />
+  <a
+    href={url} // URLを指定
+    target="_blank" // 新しいタブで開く
+    rel="noopener noreferrer" // セキュリティのために追加
+    className="block w-[351px] h-[326px] relative drop-shadow-2xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+  >
+    <img className="w-[351px] h-[204px] absolute" src={thumbnail} alt={title} />
+    
     <div className="w-[351px] h-[122px] left-0 top-[204px] absolute bg-zinc-600 shadow">
       <div className="left-[21px] top-[15px] absolute text-gray-200 text-[19px] font-bold font-['Inter'] leading-7">
         {title}
@@ -18,7 +26,7 @@ const ServiceCard = ({
       </div>
       <div className="w-4 h-4 left-[311px] top-[20px] absolute" />
     </div>
-  </div>
+  </a>
 );
 
 export default ServiceCard;
--- components/works.tsx
+++ components/works.tsx
@@ -4,8 +4,8 @@ import Heading from "./heading";
 
 const Works = () => {
   return (
-    <div className="self-stretch px-44 py-14 bg-zinc-100 flex-col justify-center items-center gap-14 flex" id="works">
-      <div className="self-stretch h-20 flex-col justify-center items-start gap-1 flex">
+    <div className="self-stretch h-[629px] px-[170px] py-[60px] bg-neutral-50 flex-col justify-center items-center gap-[60px] inline-flex" id="works">
+    <div className="self-stretch h-[83px] flex-col justify-center items-start gap-[5px] flex">
         <Heading id="worksHeading" title="WORKS" subtitle="制作実績"></Heading>
         <div className="w-28 h-px border-4 border-black"></div>
       </div>
@@ -14,27 +14,15 @@ const Works = () => {
         <ScrollContainer>
           <div className="flex gap-x-10">
             <WorksCard
-              url="https://google.com/"
-              thumbnail="https://via.placeholder.com/400x298"
-              title="協力事例"
+              url="https://replit.com/@Amplil/next-blog-editor#README.md"
+              thumbnail="https://cdn.pixabay.com/photo/2016/11/19/14/16/man-1839500_1280.jpg"
+              title="next.jsブログエディタ"
             ></WorksCard>
 
             <WorksCard
-              url="https://google.com/"
-              thumbnail="https://via.placeholder.com/400x298"
-              title="キャプションキャプションキャプションキャプションキャプション"
-            ></WorksCard>
-
-            <WorksCard
-              url="https://google.com/"
-              thumbnail="https://via.placeholder.com/400x298"
-              title="キャプションキャプションキャプションキャプションキャプション"
-            ></WorksCard>
-
-            <WorksCard
-              url="https://google.com/"
-              thumbnail="https://via.placeholder.com/400x298"
-              title="キャプションキャプションキャプションキャプションキャプション"
+              url="https://github.com/Amplil/np4g"
+              thumbnail="https://media.istockphoto.com/id/888477728/ja/%E3%82%B9%E3%83%88%E3%83%83%E3%82%AF%E3%83%95%E3%82%A9%E3%83%88/%E6%8A%BD%E8%B1%A1%E7%9A%84%E3%81%AA%E3%83%87%E3%82%B8%E3%82%BF%E3%83%AB%E3%83%8D%E3%83%83%E3%83%88%E3%83%AF%E3%83%BC%E3%82%AF%E3%82%B3%E3%83%9F%E3%83%A5%E3%83%8B%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3.jpg?s=2048x2048&w=is&k=20&c=AdmNweWyQsbUvferKBPohx4Tu7HDhcFJ5Hh3ldEC-GE="
+              title="NP4G【一般化のためのネットワークプログラミング】"
             ></WorksCard>
           </div>
         </ScrollContainer>
